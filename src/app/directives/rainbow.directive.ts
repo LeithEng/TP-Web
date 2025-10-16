caaec831b7da0e558159dd4e06a4f79b6b82f74a
MedSahbiBenKalia <mohamedsahbi.benkalia@insat.ucar.tn>
2025-10-16 01:07:14 +0100

    rainbow directive

--- src/app/directives/rainbow.directive.ts
+++ src/app/directives/rainbow.directive.ts
@@ -1,23 +1,38 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, HostBinding, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
 
 @Directive({
   selector: '[appRainbow]',
 })
-export class RainbowDirective {
+export class RainbowDirective implements OnInit, OnDestroy {
 
-  constructor() { }
+  private sub! : Subscription ;
 
   private colors: string[] = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
   @HostBinding('style.color') color: string = 'black';
   @HostBinding('style.border-color') borderColor: string = 'black';
+
+  color$ = new BehaviorSubject<string>('black');
   
   @HostListener('keyup') onKeyUp() {
-    this.color = this.getRandomColor();
-    this.borderColor = this.getRandomColor();
+    this.color$.next(this.getRandomColor());
   }
 
   getRandomColor(): string {
     const randomIndex = Math.floor(Math.random() * this.colors.length);
     return this.colors[randomIndex];
   }
+
+  ngOnInit(): void {
+    this.sub = this.color$.subscribe(color => {
+      console.log(color);
+      this.color = color;
+      this.borderColor = color;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
 }
